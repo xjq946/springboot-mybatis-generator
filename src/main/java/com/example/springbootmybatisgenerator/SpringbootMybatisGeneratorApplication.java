bef5a36f2511803b92d67c4d44d9b85826af2cb5
xiejiqing <xiejiqing@ky-tech.com.cn>
2019-10-22 16:29:52 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootmybatisgenerator/SpringbootMybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmybatisgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisGeneratorApplication.class, args);
+    }
+
+}
